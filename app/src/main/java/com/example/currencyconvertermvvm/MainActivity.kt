f7f70daaece7dccd7683c369abb84cde0851248e
Sehar Batool <97355349+seharbat00l@users.noreply.github.com>
2022-12-26 21:02:09 +0500

    initial commit with problem

---
+++ app/src/main/java/com/example/currencyconvertermvvm/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.currencyconvertermvvm
+
+import android.app.Activity
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
+/*
+import com.example.currencyconvertermvvm.databinding.ActivityMainBinding
+*/
+
+
+class MainActivity : AppCompatActivity() {
+
+//    private lateinit var binding: ActivityMainBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
